80baf8c28867c02c232c2d48d636d9e47344d510
jissjames <113381566@umail.ucc.ie>
2017-10-24 09:29:16 +0000

    A simple signup example

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Signup</title>
+    </head>
+    
+    <body>
+        <h1>Please Signup</h1>
+        <br/>
+        
+        <form action="result.php" method="POST" name="result">
+            <label for="username">Username: </label>
+            <input type="text" name="username"/>
+            <label for="email">Email:</label>
+            <input type="email" name="email"/>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
---
+++ result.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Thanks</title>
+    </head>
+    
+    <body>
+        
+        <h1>Thanks for joining us!</h1>
+        <br/>
+        
+        <?php
+            echo "Welcome, ".$_POST["username"]."<br/>";
+            echo "Your email is: ".$_POST["email"];
+        ?>
+        
+    </body>
+</html>
